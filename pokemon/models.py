bbc0b4a4ef23f9aba403449a37e63b351c135eb9
Yousefq80 <114352485+Yousefq80@users.noreply.github.com>
2022-10-11 19:58:04 +0300

    update

--- pokemon/models.py
+++ pokemon/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+name =models.CharField(max_lenght=30)
+ type =models.CharField(max_lenght=30,choices=PokemonType,default="ST")
+ hp=models.PositiveIntegerField()
+ active =models.BooleanField(default=True)
+ name_fr= models/CharField(max_length=30, default="") 
+ name_ar=models.CharField(max_length=30, default="")
+ name_jp=models.CharField(max_length=30, default="")
+ created_at=models.DateTimeField(auto_now_add=True)
+ modified_at=models.DateTimeField(auto_now=True)
 # Create your models here.
