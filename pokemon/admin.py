72d11dc77e94b31f9c067609062df564e1f7e576
Yousefq80 <ysalharby@gmail.com>
2022-10-11 21:12:12 +0300

    done

--- pokemon/admin.py
+++ pokemon/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Pokemon
+
+# Register your models here.
+class PokemonAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "hp", "active",)
+    list_filter = ("active",)
+    list_display_links = ("id", "name",)
+    #fieldsets = ({"fields":})
+    fieldsets = (
+     ('', {
+            'fields': ('name', 'type', 'hp', 'active', 'created_at', 'models_at',),
+        }),
+        
+    ('localization', {
+            'classes': ('collapse',),
+            'fields': ('name_fr', 'name_ar' ,'name_jp'),
+        }),
+               )
+
+
 
 # Register your models here.
