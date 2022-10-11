38c9360824a74ad5b8412ea4b6fe3ec92b8763ad
Yousefq80 <ysalharby@gmail.com>
2022-10-11 20:27:44 +0300

    some correction

--- pokemon/models.py
+++ pokemon/models.py
@@ -13,12 +13,12 @@ class Pokemon(models.Model):
       FAIRY = 'FA'
 
  name =models.CharField(max_length=30)
- type =models.CharField(max_length=30,choices=PokemonType,default="ST")
+ type =models.CharField(max_length=30,choices=PokemonType,default='ST')
  hp=models.PositiveIntegerField()
  active =models.BooleanField(default=True)
- name_fr= models/CharField(max_length=30, default="") 
- name_ar=models.CharField(max_length=30, default="")
- name_jp=models.CharField(max_length=30, default="")
+ name_fr= models.CharField(max_length=30,default="") 
+ name_ar=models.CharField(max_length=30,default="")
+ name_jp=models.CharField(max_length=30,default="")
  created_at=models.DateTimeField(auto_now_add=True)
  modified_at=models.DateTimeField(auto_now=True)
 
