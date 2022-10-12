dbbf16c90794a206362e51d2f5d28cc86723feb5
Yousefq80 <ysalharby@gmail.com>
2022-10-12 08:24:44 +0300

    done

--- pokemon/models.py
+++ pokemon/models.py
@@ -1,9 +1,10 @@
 from email.policy import default
 from random import choices
 from time import timezone
-
+from datetime import datetime
 from django.db import models
 from django.forms import CharField
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Pokemon(models.Model):
  class PokemonType(models.TextChoices):
@@ -15,10 +16,10 @@ class Pokemon(models.Model):
 
  name =models.CharField(max_length=30)
  type =models.CharField(max_length=30,choices=PokemonType.choices)
- hp=models.PositiveIntegerField()
+ hp=models.PositiveIntegerField(validators=[MinValueValidator(50, 'can not be under 50'), MaxValueValidator(350, 'can not be more than 350')])
  active =models.BooleanField(default=True)
  name_fr= models.CharField(max_length=30,default="",blank=True) 
  name_ar=models.CharField(max_length=30,default="",blank=True)
  name_jp=models.CharField(max_length=30,default="",blank=True)
- created_at=models.DateTimeField()
+ created_at=models.DateTimeField(default=datetime.now(), blank=True)
  modified_at=models.DateTimeField(auto_now=True)
